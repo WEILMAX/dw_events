636e3faac09b0c9376201ff2a2fac8f39fa73df2
MAXIMILLIAN WEIL <maximillian.weil@vub.be>
2023-01-20 18:14:47 +0100

    Initial changes

---
+++ dw_events/dw_events/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for dw_events."""
+
+__author__ = """Maximillian Weil"""
+__email__ = "maximillian.f.weil@gmail.be"
+__version__ = "0.1.0"
--- dw_events/setup.py
+++ dw_events/setup.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
 from setuptools import find_packages, setup
+
+requirements = ['pandas', 'scikit-learn',  'dataclasses']
+test_requirements = ['pytest>=3', ]
 
 setup(
     name="dw_events",
-    packages=find_packages(),
+    author_email='maximillian.f.weil@gmail.be',
+    python_requires='>=3.6',
+    packages=find_packages(include=['dw_events', 'dw_events.*']),
+    install_requires=requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/WEILMAX/dw_events',
     version="0.1.0",
     description="Data analytics to detect events in structural data.",
     author="Maximillian Weil",
