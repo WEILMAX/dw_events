f3e7b06e80bbb6e1ea70cce59847ab58ca63cb99
MAXIMILLIAN WEIL <maximillian.weil@vub.be>
2023-02-15 10:00:19 +0100

    Temperature anomaly

--- dw_events/dw_events/data/statistics_dataset.py
+++ dw_events/dw_events/data/statistics_dataset.py
@@ -19,21 +19,47 @@ class StatisticsGetterSCB:
     user: str = 'maximillian.weil@vub.be'
     password: str = cast(str, os.getenv('SCB_API_PASSWORD'))
     root: str = r"https://api.smartcircularbridge.eu/api/v1/"
+    location: str = 'scbalm'
     
-
     def __post_init__(self):
         self.api:API =  API(api_root=self.root, username=self.user, password=self.password)
+    
+    def get_metrics(self) -> list[str]:
+        """_summary_
 
-    def get_statistics(self):
-        new_data = self.api.query(
-                        period,
-                        location=self.location,
-                        metrics=metrics
-                        ).resample(
-                            sample_freq,
-                            offset='5T'
-                            ).last().resample(
-                                sample_freq,
-                                closed='left',
-                                label='right'
-                                ).last()#.resample(sample_freq, closed='right').last()
+        Returns:
+            _type_: _description_
+        """
+        self.metrics = self.api.metrics(locations=[self.location])
+        return self.metrics
+
+    def select_metrics(
+        self,
+        selection_crietria:list[str]
+        ) -> list[str]:
+        """_summary_
+
+        Args:
+            selection_crietria (list[str]): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        self.get_metrics()
+        metrics = self.metrics
+        selected_metrics = metrics['metric']
+        for selection_crietrium in selection_crietria:
+            selected_metrics = [m for m in selected_metrics if selection_crietrium in m]
+        return selected_metrics
+
+    def get_fbg_metrics(
+        self,
+        selection_criteria = ['FBG']
+        ) -> list[str]:
+        fbg_metrics = \
+            self.select_metrics(
+                selection_crietria=selection_criteria
+            )
+        return fbg_metrics
+
+    
